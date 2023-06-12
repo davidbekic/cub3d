54f13d3046e402400fbc114d9825480e76c267cf
davidbekic <itstijuanababy@gmail.com>
2023-06-12 19:23:20 +0200

    clean

--- build/src/init/ft_init_vars.c.d
+++
@@ -1,6 +0,0 @@
-build/./src/init/ft_init_vars.c.o: src/init/ft_init_vars.c \
-  src/init/../../include/cub3d.h include/../mlx/mlx.h
-
-src/init/../../include/cub3d.h:
-
-include/../mlx/mlx.h:
--- build/src/utils/get_next_line/get_next_line.c.d
+++
@@ -1,5 +0,0 @@
-build/./src/utils/get_next_line/get_next_line.c.o: \
-  src/utils/get_next_line/get_next_line.c \
-  src/utils/get_next_line/get_next_line.h
-
-src/utils/get_next_line/get_next_line.h:
