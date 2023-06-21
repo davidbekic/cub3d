2d9b220a2fd16e06fff9586f054894e09820c74c
davidbekic <itstijuanababy@gmail.com>
2023-06-21 12:35:27 +0200

    printing to stderr

--- src/exit/ft_exit.c
+++ src/exit/ft_exit.c
@@ -3,21 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: davidbekic <davidbekic@student.42.fr>      +#+  +:+       +#+        */
+/*   By: dbekic <dbekic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 17:23:25 by davidbekic        #+#    #+#             */
-/*   Updated: 2023/06/20 10:09:20 by davidbekic       ###   ########.fr       */
+/*   Updated: 2023/06/21 12:35:12 by dbekic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d.h"
 
-void	ft_exit(t_data *d, char *message, int flag)
+void    ft_exit(t_data *d, char *message, int flag)
 {
-	if (flag == 1)
-		printf("Error\n%s\n", message);
-	else
-		printf("%s\n", message);
-	exit(flag);
-	printf("print something of d struct: %d\n", d->keys.up);
+
+    if (flag == 1)
+    {
+        write(2, "Error\n", 6);
+        write(2, message, ft_strlen(message));
+        write(2, "\n", 1);
+    }
+    else
+    {
+        write(2, message, ft_strlen(message));
+        write(2, "\n", 1);
+    }
+    exit(flag);
+    printf("print something of d struct: %d\n", d->keys.up);
 }
+
+
